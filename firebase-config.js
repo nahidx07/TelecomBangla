286b442b90d41b2810d6f9d54e9dda8255ef8824
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-15 18:12:57 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,18 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCbYJQ9y9CJKiT5aJyrIA0pFRGy1uvZikA",
+  authDomain: "telicombangla-1.firebaseapp.com",
+  projectId: "telicombangla-1",
+  storageBucket: "telicombangla-1.firebasestorage.app",
+  messagingSenderId: "1020351520674",
+  appId: "1:1020351520674:web:c84659bc5ca2f6b41edcfb"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export { signInWithPopup };
